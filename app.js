fcf55b7c363724cac183d3006772dc8fb21fd400
LameboiHari <60195485+LameboiHari@users.noreply.github.com>
2022-02-03 16:47:31 -0500

    Msg

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
+const PREFIX = '!';
+
 //Load bot token from .env file
 require("dotenv").config({path:'./config/keys.env'});
 
@@ -12,6 +14,23 @@ client.on("messageCreate", message => {
     if (message.content == 'hi') {
         message.reply(`Hello ${message.author.username}!`);
     }
+})
+
+client.on('message', message => {
+    
+    let args = message.contenet.substring(PREFIX.length).split(" ");
+
+    switch (args[0]) {
+        
+        case 'play':
+
+        if (!args[1]) {
+            message.channel.send("Please provide a valid youtube link!");
+        }
+
+        break;
+    }
+
 })
 
 client.login(process.env.BOT_TOKEN);
